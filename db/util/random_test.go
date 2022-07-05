ccb547a99ed3f093484ded1d763ad2b686c414ce
hmhuan <minhhuanhuynh289@gmail.com>
2022-07-06 00:55:09 +0900

    Update random_test.go

--- db/util/random_test.go
+++ db/util/random_test.go
@@ -14,4 +14,29 @@ func TestRandomInt(t *testing.T) {
 	require.True(t, random <= max)
 }
 
+func TestRandomString(t *testing.T) {
+	length := 10
+	random := RandomString(length)
 
+	require.Equal(t, length, len(random))
+}
+
+func TestRandomOwner(t *testing.T) {
+	owner1 := RandomOwner()
+
+	require.Equal(t, 10, len(owner1))
+}
+
+func TestRandomBalance(t *testing.T) {
+	balance := RandomBalance()
+
+	require.True(t, balance >= 100)
+	require.True(t, balance <= 1000)
+}
+
+func TestRandomCurrency(t *testing.T) {
+	currencies := []string{"USD", "JPY", "VND", "CAD", "EUR"}
+	random := RandomCurrency()
+
+	require.Contains(t, currencies, random)
+}
